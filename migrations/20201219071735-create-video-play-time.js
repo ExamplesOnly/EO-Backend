785e274f1b5484b4e9f5ff387ecefd511933ecb5
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2020-12-19 21:17:59 +0530

    Add video PlayTime count on model
    Signed-off-by: Bishwajyoti Roy <bishwajyoti456@gmail.com>

--- migrations/20201219071735-create-video-play-time.js
+++ migrations/20201219071735-create-video-play-time.js
@@ -17,6 +17,9 @@ module.exports = {
       userId: {
         type: Sequelize.INTEGER,
       },
+      playTime: {
+        type: Sequelize.INTEGER,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
